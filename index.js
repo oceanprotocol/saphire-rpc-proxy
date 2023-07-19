1506c5ae7823d9153903d64bdca944a0c7342dca
alexcos20 <alex.coseru@gmail.com>
2023-07-19 20:48:26 +0300

    add fixes

--- index.js
+++ index.js
@@ -6,7 +6,8 @@ const app = express();
 app.use(express.json());
 
 const provider = sapphire.wrap(new ethers.providers.JsonRpcBatchProvider(process.env.RPC));
-
+const archiveNode = process.env.ARCHIVE_NODE ? true: false
+const useBlockNo = process.env.USE_BLOCK_NO ? true: false
 app.post('/', async (req, res) => {
   if (req.body.jsonrpc !== '2.0') {
     res.status(405).end();
@@ -17,12 +18,30 @@ app.post('/', async (req, res) => {
   let { id, method, params } = req.body;
   if (method === 'eth_call') {
     const { gas, ...txCall } = params[0];
-    result = await provider.call({ from: ethers.constants.AddressZero, ...txCall }, params[1]);
+    let callBlock = "latest"
+    if (archiveNode){
+      if (useBlockNo){
+        const fullblock = await provider.getBlock(params[1]["blockHash"])
+        callBlock = fullblock["number"]
+      }
+      else{
+        callBlock = params[1]["blockHash"]
+      }
+    }
+    const p1={ from: ethers.constants.AddressZero, ...txCall }
+    
+    try{
+      result = await provider.call(p1, callBlock);
+      res.status(200).json({ id, jsonrpc: '2.0', result }).end();
+    }
+    catch(error){
+      console.error("*******************************\nFailed call for request "+id+":\n"+JSON.stringify(p1)+"\n Error:\n"+JSON.stringify(error)+"*******************************\n")
+      res.status(500).json(error).end();
+    }
   } else {
     result = await provider.send(method, params);
+    res.status(200).json({ id, jsonrpc: '2.0', result }).end();
   }
-
-  res.status(200).json({ id, jsonrpc: '2.0', result }).end();
 });
 
 app.listen(8080);
